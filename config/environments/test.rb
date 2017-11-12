1eb5040b52d98fd3eb37a845e638b368190fe855
Ryan Popovic <rjp203@hotmail.com>
2017-11-12 18:23:15 +0000

    User Story 2 User Sign Up: Completed

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.default_url_options = { host: 'https://blocmarks-rjp203.c9users.io' }
+
+  Rails.application.routes.default_url_options[:host] = 'https://blocmarks-rjp203.c9users.io'
 end
